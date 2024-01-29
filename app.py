9e13798eb17ea30c9fc4ffcbea0d433c0214c448
Nithin G <79520840+2nithing@users.noreply.github.com>
2024-01-29 07:09:07 +0000

    2

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pickle
 
 with st.form("myform"):
-    height = st.text_input(label="Enter Height in inches")
+    height = st.number_input(label="Enter Height in inches",min_value=32.0,max_value=120.0,value=50.00)
     submitted  = st.form_submit_button("Submit")
     print(type(height))
 if submitted:
         model = pickle.load(open('./model.pkl','rb'))
-        weight = model.predict([[int(height)]])
+        weight = model.predict([[height]])
         st.write("weight value is ", round(weight[0],2)," pounds")
