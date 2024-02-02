28963210f465060fa9470044faced3c77cd17275
Nithin G <79520840+2nithing@users.noreply.github.com>
2024-02-02 15:53:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 
 st.header("Predict weight from height")
-st.write("demo of machine learning model")
+st.write("Demo of linear regression machine learning model")
 with st.form("myform"):
     height = st.number_input(label="Enter Height in inches",min_value=32.0,max_value=120.0,value=50.00,step=1.0)
     submitted  = st.form_submit_button("Submit")
